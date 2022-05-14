fddbd5328153ec748a79e7210317b11dba56dfbc
Shadow0K <scarletshadow0k@gmail.com>
2022-05-14 10:50:23 -0700

    Basic structure of the mod

--- src/main/java/com/shadow/zero/item/ModItems.java
+++ src/main/java/com/shadow/zero/item/ModItems.java
@@ -16,6 +16,8 @@ public class ModItems {
     //cobalt ingot
     public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static final RegistryObject<Item> COBALT_NUGGET = ITEMS.register("cobalt_nugget", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
 
 
 
